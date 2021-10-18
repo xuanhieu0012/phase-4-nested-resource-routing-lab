fe8c44b7865ed653eda1b40c10f0c1887fd7fd8c
Hieu <xuanhieu0012@gmail.com>
2021-10-18 00:10:10 -0500

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_error_message
 
-  def index
-    items = Item.all
-    render json: items, include: :user
+  def index 
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items
+    else
+      item = Item.all
+    end
+    render json: item, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+      render json: item, status: 201
+  
+  end
+
+ 
+
+  private 
+
+  def render_error_message
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
