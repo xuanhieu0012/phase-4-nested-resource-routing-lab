0fa1045aa1a9f4081e552a20af456a950fe7afcf
Hieu <xuanhieu0012@gmail.com>
2021-10-18 00:09:38 -0500

    commit tree d1e9646929115e597bfcf92cff904c05950ac1a4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_error_message
+  
+
 
   def show
+   
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+  # private 
+
+  # def render_error_message
+  #   render json: { error: "User not found" }, status: :not_found
+  # end
 
 end
